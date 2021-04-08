8bee40bed9f45fd700b5d79da34898d6dcc8b120
Shiguruji <80973804+Shiguruji@users.noreply.github.com>
2021-04-08 16:40:31 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const navLINKS = document.getElementById("nav-links");
-
-hamburger.addEventListener("click", () => {
-  navLINKS.classList.toggle("show");
-});
- 
